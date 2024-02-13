c69d4da0d1e04548429539fda91321849ce02125
omarhamdi27 <80309615+omarhamdi27@users.noreply.github.com>
2024-02-13 11:18:38 +0100

    Testing commit

--- pidev1/src/main/java/org/example/Main.java
+++ pidev1/src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-
+        System.out.println("Hello world!");
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         /*DataSource ds = DataSource.getInstance();
         System.out.println(ds);*/
