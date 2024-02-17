b59447b9cf1addb07ba43964e23df78f1a1ef337
sarrafekih1 <fekih.sarra@esprit.tn>
2024-02-17 19:29:14 +0100

    addexposition

--- src/main/java/edu/esprit/controllers/AjouterExposition.java
+++ src/main/java/edu/esprit/controllers/AjouterExposition.java
@@ -16,38 +16,37 @@ import java.sql.Timestamp;
 import java.time.LocalDate;
 
 public class AjouterExposition {
-//    private final ServiceExposition PS = new ServiceExposition();
+    private final ServiceExposition exp = new ServiceExposition();
+
+    @FXML
+    public Button buttonaddExpoID;
+    @FXML
+    public DatePicker datefinId;
+    @FXML
+    public DatePicker dateDebutId;
+    @FXML
+    public TextField ImageId;
+    @FXML
+    public TextField themeID;
+    @FXML
+    public TextField descriptionId;
+    @FXML
+    public TextField nomExpoId;
+    public void addExpo(ActionEvent event)throws IOException {
 //
-//    @FXML
-//    public Button buttonaddExpoID;
-//    @FXML
-//    public DatePicker datefinId;
-//    @FXML
-//    public DatePicker dateDebutId;
-//    @FXML
-//    public TextField ImageId;
-//    @FXML
-//    public TextField themeID;
-//    @FXML
-//    public TextField descriptionId;
-//    @FXML
-//    public TextField nomExpoId;
-//
-//    public void addExpo(ActionEvent event) {
-//
-//            LocalDate startDate = dateDebutId.getValue();
-//            LocalDate endDate = datefinId.getValue();
-//
-//            Timestamp sqlStartDate = Timestamp.valueOf(startDate.atStartOfDay());
-//            Timestamp sqlEndDate = Timestamp.valueOf(endDate.atStartOfDay());
-//
-//            PS.ajouter(new Exposition(
-//                    nomExpoId.getText(),
-//                    sqlStartDate,
-//                    sqlEndDate,
-//                    descriptionId.getText(),
-//                    themeID.getText(),
-//                    ImageId.getText()));
+            LocalDate startDate = dateDebutId.getValue();
+            LocalDate endDate = datefinId.getValue();
+
+            Timestamp sqlStartDate = Timestamp.valueOf(startDate.atStartOfDay());
+            Timestamp sqlEndDate = Timestamp.valueOf(endDate.atStartOfDay());
+
+            exp.ajouter(new Exposition(
+                    nomExpoId.getText(),
+                    sqlStartDate,
+                    sqlEndDate,
+                    descriptionId.getText(),
+                    themeID.getText(),
+                    ImageId.getText()));
 //
 //            Alert alert = new Alert(Alert.AlertType.INFORMATION);
 //            alert.setTitle("Validation");
@@ -55,6 +54,6 @@ public class AjouterExposition {
 //            alert.showAndWait();
 //
 //
-//    }
+   }}
 
-}
+
