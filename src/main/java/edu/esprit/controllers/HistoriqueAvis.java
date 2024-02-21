fcb0513321ab20bbc6520a4c4af85861419db96d
aziz9hamouda <mohamedaziz.hamouda@esprit.tn>
2024-02-21 01:26:26 +0100

    suppression_avis

--- src/main/java/edu/esprit/controllers/HistoriqueAvis.java
+++ src/main/java/edu/esprit/controllers/HistoriqueAvis.java
@@ -5,11 +5,16 @@ import edu.esprit.services.ServiceAvis;
 import edu.esprit.services.ServiceOeuvre;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import java.sql.Date;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
+
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -31,12 +36,21 @@ public class HistoriqueAvis {
     @FXML
     private TableColumn<Avis, String> nomoeuvre_id;
 
+    @FXML
+    private Button buttonsupprimer_id;
 
+    ServiceAvis serviceAvis = new ServiceAvis();
     @FXML
     public void initialize() {
+
+        TableView.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
+               // handleTableViewDoubleClick();
+            }
+        });
         // Get the user's reviews
-        ServiceAvis serviceAvis = new ServiceAvis();
-        ObservableList<Avis> avisList = FXCollections.observableArrayList(serviceAvis.getAvisByUserId(3)); // Replace 4 with the actual user ID
+
+        ObservableList<Avis> avisList = FXCollections.observableArrayList(serviceAvis.getAvisByUserId(4)); // Replace 4 with the actual user ID
 
         // Set up the columns in the table
         Note_id.setCellValueFactory(new PropertyValueFactory<Avis, Integer>("note"));
@@ -47,6 +61,23 @@ public class HistoriqueAvis {
         // Load the data into the table
         TableView.setItems(avisList);
     }
+    @FXML
+    private void delete(ActionEvent event) throws SQLException {
+        Avis selectedRec = (Avis) TableView.getSelectionModel().getSelectedItem();
+        if (selectedRec != null) {
+            // Delete from database
+            serviceAvis.supprimer(selectedRec.getIdAvis());
+
+            // Refresh table view
+            initialize();
+        }
+    }
+    //private void handleTableViewDoubleClick() {
+      //  Avis selectedRec = TableView.getSelectionModel().getSelectedItem();
+       // if (selectedRec != null) {
+            //openEditDialog(selectedRec);
+       // }
+    //}
 
 
 }
