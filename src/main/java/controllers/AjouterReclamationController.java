c5e482ca8153c1ea0c43891606e500f8a72e2bbc
khalil1702 <khalil.chekili@esprit.tn>
2024-02-23 00:14:41 +0100

    afficher reclamation

--- src/main/java/controllers/AjouterReclamationController.java
+++ src/main/java/controllers/AjouterReclamationController.java
@@ -5,8 +5,13 @@ import entities.User;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import service.ReclamationService;
 import service.ServiceUser;
 
@@ -55,7 +60,8 @@ public class AjouterReclamationController {
 
     @FXML
     private Button supprimer;
-
+    @FXML
+    private Button rec;
     public void initialize() throws IOException {
         ShowReclamation();
 
@@ -148,5 +154,20 @@ public class AjouterReclamationController {
         TableViewRec.getItems().remove(selectedReclamation);
     }
 
+    @FXML
+    void rec(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/AfficherReclamations.fxml"));
+        Parent root = loader.load();
 
+        // Créer une nouvelle scène
+        Scene scene = new Scene(root);
+
+        // Configurer la nouvelle scène dans une nouvelle fenêtre
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Reclamations");
+
+        // Afficher la nouvelle fenêtre
+        stage.show();
+    }
 }
